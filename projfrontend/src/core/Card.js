1280851b5ee3b8e969f99ff20bc3b588d3a1b94a
shikhar agarwal <shikharagarwal2807@gmail.com>
2020-04-10 14:42:50 +0530

    tshirts display

--- projfrontend/src/core/Card.js
+++ projfrontend/src/core/Card.js
@@ -3,6 +3,10 @@ import ImageHelper from "./helper/ImageHepler";
 
 const Card = ({product, addToCart = true, removeFromCart = false}) => {
 
+    const cartTitle = product ? product.name : "A photo from pexels";
+    const cartDescription = product ? product.description : "Default Description";
+    const cartPrice = product ? product.price : "Default";
+
     const showAddtoCart = addToCart => {
         return (
             addToCart && (
@@ -34,15 +38,15 @@ const Card = ({product, addToCart = true, removeFromCart = false}) => {
 
     return (
         <div className="card text-white bg-dark border border-info ">
-            <div className="card-header lead">A photo from pexels</div>
+            <div className="card-header lead">{cartTitle}</div>
             <div className="card-body">
                 <div className="rounded border border-success p-2">
                     <ImageHelper product={product}/>
                 </div>
                 <p className="lead bg-success font-weight-normal text-wrap p-3">
-                    this photo looks great
+                    {cartDescription}
                 </p>
-                <h4 className="btn btn-success rounded  btn-sm px-4">$ 5</h4>
+                <h4 className="btn btn-success rounded  btn-sm px-4">$ {cartPrice}</h4>
                 <div className="row">
                     <div className="col-12">
                         {showAddtoCart(addToCart)}
--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../styles.css";
 import Base from "./Base";
 import Card from "./Card";
+import {getAllProducts} from "../admin/helper/adminapicall";
 
 export default function Home() {
+
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(false);
+
+    const loadAllProducts = () => {
+        getAllProducts().then(data => {
+            if (data.error) {
+                setError(data.error)
+            } else {
+                setProducts(data);
+            }
+        });
+    };
+
+    useEffect(() => {
+        loadAllProducts()
+    }, []);
+
+
     return (
-        <Base title="Home Page" description="Welcome to the Tshirt Store">
-            <div className="row text-center">
-                <div className="col-4">
-                    <Card />
-                </div>
-                <div className="col-4">
-                    <Card />
-                </div>
-                <div className="col-4">
-                    <Card />
+        <Base title="Home Page" description="Welcome to the T-shirt Store">
+            <div className="row">
+                <h1 className="text-white text-center">All Products</h1>
+                <div className="row">
+                    {products.map((product, index) => {
+                        return (
+                            <div key={index} className="col-4 mb-4 text-center">
+                                <Card product={product}/>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </Base>
